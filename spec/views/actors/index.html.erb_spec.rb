b783b2cb23ed29391c41d94a33c0d8d276526baf
Justin Valentini <justin.valentini@gmail.com>
2013-04-16 01:11:16 -0400

    Fixing test

--- spec/views/actors/index.html.erb_spec.rb
+++ spec/views/actors/index.html.erb_spec.rb
@@ -10,7 +10,7 @@ describe "actors/index" do
         :url => "Url"
       ),
       stub_model(Actor,
-        :login => "Login",
+        :login => "Login2",
         :avatar_url => "Avatar Url",
         :gravatar_id => "Gravatar",
         :url => "Url"
@@ -21,7 +21,7 @@ describe "actors/index" do
   it "renders a list of actors" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Login".to_s, :count => 2
+    assert_select "tr>td", :text => "Login*".to_s, :count => 2
     assert_select "tr>td", :text => "Avatar Url".to_s, :count => 2
     assert_select "tr>td", :text => "Gravatar".to_s, :count => 2
     assert_select "tr>td", :text => "Url".to_s, :count => 2
