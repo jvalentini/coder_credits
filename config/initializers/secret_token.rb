be9088b746344ee02d5d4db940793d799edb0679
Justin Valentini <justin.valentini@gmail.com>
2013-04-16 00:38:00 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoderCredits::Application.config.secret_token = 'aa83b59ad7a5d0b6b66b6ec30ac09fe8287190f65395d2146a709e7679211a33fe3050406218dc0a53ce21e5bb3e1c570395d3a529f74c48e1ced17405c094d9'
